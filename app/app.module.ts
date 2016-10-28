175e0cbfe59ab6f97358733c5e68bbe4f3b8ca5e
Zenn <app.zenn@gmail.com>
2016-10-29 02:44:59 +0900

    Implementing Chapter Http
    InMemoryDataServiceが上手く動かなかったので、ローカルでサーバ建ててやってる

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent }   from './app.component';
 import { HeroesComponent } from './heroes.component';
@@ -8,12 +11,12 @@ import { HeroDetailComponent } from './hero-detail.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroService } from './hero.service';
 
-import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     AppRoutingModule
   ],
   declarations: [
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'http://localhost:4000/heroes'; // URL for web api
+
+  constructor(private http: Http) {}
+
   getHeroes() : Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
   }
 
   getHero(id: number): Promise<Hero> {
@@ -17,6 +26,11 @@ export class HeroService {
     return new Promise<Hero[]>(resolve =>
       setTimeout(resolve, 2000)) // delay 2 seconds
       .then(() => this.getHeroes());
+  }
+
+  handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 
 }
